b65011e207e2864df987b14293130319a31c7899
Wojciechprg <rabiza966@gmail.com>
2021-11-19 17:58:16 +0100

    Update main.py

--- tele_bot/main.py
+++ tele_bot/main.py
@@ -1,4 +1,3 @@
-
 import datetime
 import requests
 from bs4 import BeautifulSoup
@@ -12,25 +11,29 @@ KEY = os.getenv('API_KEY')
 bot = telebot.TeleBot(KEY)
 
 sosy = ('jalapeno', '1000 wysp', 'firecracker', 'algierski', 'musztarda bbq', 'bbq', 'slodkie chili', 'amerykanski',
-            'serowy', 'paprykowy sriracha', 'arabski', 'curry&mango', 'czosnkowy', 'ketchup', 'musztarda', 'remoulada',
-            'andaluzyjski', 'meksykanski')
+        'serowy', 'paprykowy sriracha', 'arabski', 'curry&mango', 'czosnkowy', 'ketchup', 'musztarda', 'remoulada',
+        'andaluzyjski', 'meksykanski')
+
 
 @bot.message_handler(commands=['sosiwo'])
 def sosiwo(message):
     bot.reply_to(message, random.choice(sosy))
 
+
 @bot.message_handler(commands=['sosy'])
 def sosiwo(message):
-    bot.reply_to(message, random.choice(sosy)+" "+ random.choice(sosy))
+    bot.reply_to(message, random.choice(sosy) + " " + random.choice(sosy))
+
 
 @bot.message_handler(commands=['hlanie'])
 def picie(message):
     decyzja = ['tak', 'nie']
     name = message.from_user.first_name
     if name == 'Michał':
-     bot.reply_to(message, "Dla pana michała nie ma picia")
+        bot.reply_to(message, "Dla pana michała nie ma picia")
     else:
-     bot.reply_to(message, random.choice(decyzja))
+        bot.reply_to(message, random.choice(decyzja))
+
 
 @bot.message_handler(commands=['gp'])
 def gp(message):
@@ -41,10 +44,11 @@ def gp(message):
     result = ends - start
     minuty = (result.seconds - (result.seconds // 3600) * 3600) // 60
     if (result.days > 7):
-        bot.reply_to(message,"no rawe ceek  this time")
+        bot.reply_to(message, "no rawe ceek  this time")
     elif (result.days <= 7):
-        bot.reply_to(message,"Do grand prix zostało " + str(result.days) + " dni, oraz " + str(
+        bot.reply_to(message, "Do grand prix zostało " + str(result.days) + " dni, oraz " + str(
             result.seconds // 3600) + " godzin i " + str(minuty) + " minuty")
+
 
 @bot.message_handler(commands=['promocja'])
 def zabka(message):
@@ -57,12 +61,11 @@ def zabka(message):
     for x in lista:
         quote['alkohol'] = x.img['alt']
         quote['cena'] = (x.select_one("span.price.product__price").text[9]) + "." + (
-        x.select_one("span.price.product__price").text[10:12])
+            x.select_one("span.price.product__price").text[10:12])
         quote['sztuki'] = x.select_one("span.badge__name").text[26]
         piwa.append(quote.copy())
-
 
     bot.send_message(message.chat.id, str(piwa))
 
 
-bot.polling()  
+bot.polling()
